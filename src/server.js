2829c1e2185e80ed6b348074713eb98783ecfb81
Miguel Angel <mige5553@gmail.com>
2019-07-02 18:52:26 +0100

    añadido nodemon

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 const express = require("express");
-
 const PORT = 3000;
-//const HOST = "0.0.0.0";
+const usuarios = [
+  { id: 0, name: "pepe", profesión: "Ratero", sexo: "Hombre" },
+  { id: 1, name: "Juana", profesión: "Ratera", sexo: "Mujer" },
+  { id: 2, name: "Aaron", profesión: "Espabilado", sexo: "Hombre" },
+  { id: 3, name: "Ale", profesión: "Norteño", sexo: "NaN" },
+  { id: 4, name: "Migue", profesión: "hola", sexo: "Hombre..." },
+  { id: 5, name: "Paca", sexo: "Mujer" },
+  { id: 6, name: "Firulays", sexo: "Gatita" }
+];
 
 const app = express();
-app.get("/", (req, res) => {
-  console.log("Request", req);
-  res.send("Hello world\n");
+
+app.get("/usuarios", (req, res) => {
+  //console.log("Request", req);
+  //res.send("Hello world\n");
+  res.json(usuarios);
 });
 
-app.listen(PORT, () => console.log(`Running on port 3000`));
+app.get("/usuarios/:id", (req, res) => {
+  const userId = req.params.id;
+  const selectedUser = usuarios.find(usuario => usuario.id == userId);
+  res.json(selectedUser);
+});
+app.listen(3000);
